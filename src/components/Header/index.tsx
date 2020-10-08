6fe6265a552d780c2807630ed42cc94ba4e1cd22
Paulo Roberto <prmorais1302@gmail.com>
2020-10-08 20:56:11 -0300

    Adicionando navbar - corrigido

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 
 const Header: React.FC = () => {
-	return (
-		<Navbar bg="dark" variant="dark">
-			<Navbar.Brand href="#home">Code83 - Tarefas</Navbar.Brand>
-			<Nav className="mr-auto">
-				<Nav.Link href="#home">Início</Nav.Link>
-				<Nav.Link href="#features">Tarefas</Nav.Link>
-			</Nav>
-		</Navbar>
-	);
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Code83 - Tarefas</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to={"/"}>
+            Início
+          </Nav.Link>
+          <Nav.Link as={Link} to={"/tasks"}>
+            Tarefas
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Header;
